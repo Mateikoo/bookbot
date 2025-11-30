a8eacb9ead7e76fe953bbf7e8fea38317c852b74
Mateikoo <mateuszjaroniewskimail2@gmail.com>
2025-11-30 15:44:42 +0100

    project finished

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 from stats import words_count, symbols_count, symbols_sort
 
 
@@ -12,12 +18,12 @@ def get_book_text(path):
 
 def main():
 
-    book_text = get_book_text("/home/mateusz/workspace/github.com/mateusz/bookbot/books/frankenstein.txt")
+    book_text = get_book_text(sys.argv[1])
     symbols_dict = symbols_count(book_text)
     sorted_list = symbols_sort(symbols_dict)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(words_count(book_text))
     print("--------- Character Count -------")
